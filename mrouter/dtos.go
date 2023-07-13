66e611d0bf436aed9d8cd96950bfe654875c622f
cuongnguyen4 <cuongnguyen@aiontech.vn>
2023-07-13 10:33:53 +0700

    Update Publish Req fields

--- mrouter/dtos.go
+++ mrouter/dtos.go
@@ -3,8 +3,10 @@ package mrouter
 // PublishReq is publishing/sending request
 // Eg: a publishReq instance is a Publishing Data to Redis Pub/Sub, a Request to client via Websocket
 type PublishReq struct {
-	ID   string `json:"id"`
-	Func string `json:"function"`
+	ID     string `json:"id"`
+	Type   int    `json:"type"`
+	Result int64  `json:"result"`
+	Func   string `json:"function"`
 	// Whom request refer to, like sub in jwt
 	Sub  string      `json:"sub"`
 	Data interface{} `json:"data"`
