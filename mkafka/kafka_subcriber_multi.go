413d9d6cd2a78639f846c263f7cf6b80d417e61c
cuongnguyen4 <cuongnguyen@aiontech.vn>
2025-07-28 13:43:23 +0700

    Disable check key

--- mkafka/kafka_subcriber_multi.go
+++ mkafka/kafka_subcriber_multi.go
@@ -130,23 +130,23 @@ func (_this *kafkaMultiSubscriber) Run(args *mrouter.OpenServerArgs) {
 				}
 				log.Println("[Kafka] Received msg on channel [", msg.TopicPartition, "]", string(msg.Key))
 
-				// 🧠 Deduplication check
-				msgKeyStr := string(msg.Key)
-				if msgKeyStr != "" {
-					if _, exists := _this.seenKeys.Get(msgKeyStr); exists {
-						log.Printf("[Kafka] Skipping duplicated %v: %s\n", msg.TopicPartition, msgKeyStr)
-						if _this.config.ManualCommit {
-							_, cErr := w.Consumer.CommitMessage(msg)
-							if cErr != nil {
-								log.Printf("[Kafka] Commit error for duplicated msg: %v", cErr)
-							} else {
-								log.Printf("[Kafka] Committed offset for duplicated key: %s\n", msgKeyStr)
-							}
-						}
-						continue
-					}
-					_this.seenKeys.Add(msgKeyStr, struct{}{})
-				}
+				//// 🧠 Deduplication check
+				//msgKeyStr := string(msg.Key)
+				//if msgKeyStr != "" {
+				//	if _, exists := _this.seenKeys.Get(msgKeyStr); exists {
+				//		log.Printf("[Kafka] Skipping duplicated %v: %s\n", msg.TopicPartition, msgKeyStr)
+				//		if _this.config.ManualCommit {
+				//			_, cErr := w.Consumer.CommitMessage(msg)
+				//			if cErr != nil {
+				//				log.Printf("[Kafka] Commit error for duplicated msg: %v", cErr)
+				//			} else {
+				//				log.Printf("[Kafka] Committed offset for duplicated key: %s\n", msgKeyStr)
+				//			}
+				//		}
+				//		continue
+				//	}
+				//	_this.seenKeys.Add(msgKeyStr, struct{}{})
+				//}
 
 				Semaphore <- struct{}{}
 				handled = true
